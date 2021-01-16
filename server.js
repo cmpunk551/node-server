1f80f5fb7c7972f16239237c1b7380af73138cf1
Николай Черняк <cmpunk5551@gmail.com>
2021-01-16 22:22:35 +0300

    реализация основных функций

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const paginate = require('jw-paginate');
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const users_json = fs.readFileSync('users.json')
+users = JSON.parse(users_json).users;
+
+app.get('/api/items', (req, res, next) => {
+
+    const page = parseInt(req.query.page) || 1;
+    const pager = paginate(users.length, page);
+    const pageOfItems = users.slice(pager.startIndex, pager.endIndex + 1);
+
+    return res.json({ pager, pageOfItems });
+});
+
+const port = 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
